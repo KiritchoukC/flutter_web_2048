a313a70b98e60d9ba9080355a6a6cfd70974ec65
KiritchoukC <clement.kiritchouk@gmail.com>
2019-11-09 08:01:08 +0100

    feat(get tile destination): add function to get the destination of the tile

--- lib/features/game/domain/entities/board.dart
+++ lib/features/game/domain/entities/board.dart
@@ -4,7 +4,9 @@ import 'vector.dart';
 
 class Board {
   List<List<Tile>> tiles;
+
   List<Tile> get flatTiles => tiles.expand((i) => i).toList();
+
   Board(this.tiles);
 
   /// Returns all the empty tiles in the [board]
@@ -21,14 +23,39 @@ class Board {
   /// Returns the given tile final destination and if
   /// it needs to be merged with another tile
   Destination getTileDestination(Tile tile, Vector vector) {
+    // set the initial destination with the tile position
     var destination = Destination(x: tile.x, y: tile.y, merged: false);
 
-    while (destination.x < 3 && destination.y < 3 && destination.x > 0 && destination.y > 0) {
-      destination = Destination(
-        x: destination.x + vector.x,
-        y: destination.y + vector.y,
-        merged: false,
-      );
+    while (true) {
+      int x = destination.x + vector.x;
+      // break if [x] is out of the board
+      if (x > 3 || x < 0) {
+        break;
+      }
+
+      int y = destination.y + vector.y;
+      // break if [y] is out of the board
+      if (y > 3 || y < 0) {
+        break;
+      }
+
+      var nextTile = this.tiles[x][y];
+
+      // if next tile is empty set the new destination 
+      // and continue with the next one
+      if (nextTile == null) {
+        destination = Destination(y: y, x: x, merged: false);
+        continue;
+      }
+
+      // break if next tile is not empty and have a different value
+      if (nextTile.value != tile.value) {
+        break;
+      }
+
+      // if tiles have the same value, merge them and break
+      destination = Destination(y: y, x: x, merged: true);
+      break;
     }
 
     return destination;
--- test/features/game/domain/entities/board_test.dart
+++ test/features/game/domain/entities/board_test.dart
@@ -1,12 +1,10 @@
-
-
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_web_2048/core/enums/direction.dart';
 import 'package:flutter_web_2048/features/game/domain/entities/board.dart';
 import 'package:flutter_web_2048/features/game/domain/entities/tile.dart';
 import 'package:flutter_web_2048/features/game/domain/entities/vector.dart';
 
-void main(){
-  
+void main() {
   group('getEmptyTiles', () {
     test("should have a length of 16 for a blank board", () {
       // ARRANGE
@@ -83,33 +81,486 @@ void main(){
   });
 
   group('getTileDestination', () {
-    test('should return same position if tile does not move', () {
-      // ARRANGE
-      var vector= Vector(1, 0); // moving right
-      var tiles = List<List<Tile>>.generate(4, (y) => List(4));
-      var board = Board(tiles);
+    group('no move', () {
+      test('should return same position if tile does not move (left)', () {
+        // ARRANGE
+        var vector = Vector(-1, 0);
 
-      int x = 3;
-      int y = 0;
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
 
-      var tile = Tile(2, x: x, y: y);
+        int x = 0;
+        int y = 0;
 
-      // put the tile in the board
-      board.tiles[x][y] = tile;
+        var tile = Tile(2, x: x, y: y);
 
-      // board input
-      // |0|0|0|2|
-      // |0|0|0|0|
-      // |0|0|0|0|
-      // |0|0|0|0|
-      // it should not move since the tile is already on the right side
+        // put the tile in the board
+        board.tiles[x][y] = tile;
 
-      // ACT
-      var actual = board.getTileDestination(tile, vector);
-      // ASSERT
-      expect(actual.x, x);
-      expect(actual.y, y);
-      expect(actual.merged, false);
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, x);
+        expect(actual.y, y);
+        expect(actual.merged, false);
+      });
+
+      test('should return same position if tile does not move (right)', () {
+        // ARRANGE
+        var vector = Vector(1, 0);
+
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        int x = 3;
+        int y = 0;
+
+        var tile = Tile(2, x: x, y: y);
+
+        // put the tile in the board
+        board.tiles[x][y] = tile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, x);
+        expect(actual.y, y);
+        expect(actual.merged, false);
+      });
+
+      test('should return same position if tile does not move (up)', () {
+        // ARRANGE
+        var vector = Vector(0, -1);
+
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        int x = 0;
+        int y = 0;
+
+        var tile = Tile(2, x: x, y: y);
+
+        // put the tile in the board
+        board.tiles[x][y] = tile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, x);
+        expect(actual.y, y);
+        expect(actual.merged, false);
+      });
+
+      test('should return same position if tile does not move (down)', () {
+        // ARRANGE
+        var vector = Vector(0, 1);
+
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        int x = 0;
+        int y = 3;
+
+        var tile = Tile(2, x: x, y: y);
+
+        // put the tile in the board
+        board.tiles[x][y] = tile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, x);
+        expect(actual.y, y);
+        expect(actual.merged, false);
+      });
+    });
+    group('move all the way', () {
+      test('should move all the way to the right', () {
+        // ARRANGE
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        var vector = Vector(1, 0);
+
+        int x = 0;
+        int y = 0;
+
+        var tile = Tile(2, x: x, y: y);
+
+        // put the tile in the board
+        board.tiles[x][y] = tile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, 3);
+        expect(actual.y, y);
+        expect(actual.merged, false);
+      });
+
+      test('should move all the way to the left', () {
+        // ARRANGE
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        var vector = Vector(-1, 0);
+
+        int x = 3;
+        int y = 0;
+
+        var tile = Tile(2, x: x, y: y);
+
+        // put the tile in the board
+        board.tiles[x][y] = tile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, 0);
+        expect(actual.y, y);
+        expect(actual.merged, false);
+      });
+
+      test('should move all the way down', () {
+        // ARRANGE
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        var vector = Vector(0, 1);
+
+        int x = 0;
+        int y = 0;
+
+        var tile = Tile(2, x: x, y: y);
+
+        // put the tile in the board
+        board.tiles[x][y] = tile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, x);
+        expect(actual.y, 3);
+        expect(actual.merged, false);
+      });
+
+      test('should move all the way up', () {
+        // ARRANGE
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        var vector = Vector(0, -1);
+
+        int x = 0;
+        int y = 3;
+
+        var tile = Tile(2, x: x, y: y);
+
+        // put the tile in the board
+        board.tiles[x][y] = tile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, x);
+        expect(actual.y, 0);
+        expect(actual.merged, false);
+      });
+    });
+    group('move with blocking tile', () {
+      test('should move up until blocked by another tile with a different value', () {
+        // ARRANGE
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        var vector = Vector(0, -1);
+
+        int x = 0;
+        int y = 3;
+
+        var tile = Tile(2, x: x, y: y);
+        var blockingTile = Tile(4, x: 0, y: 0);
+
+        // Starting board
+        // |4|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |2|0|0|0|
+
+        // Ending board
+        // |4|0|0|0|
+        // |2|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+
+        // put the tiles in the board
+        board.tiles[x][y] = tile;
+        board.tiles[0][0] = blockingTile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, x);
+        expect(actual.y, 1);
+        expect(actual.merged, false);
+      });
+
+      test('should move down until blocked by another tile with a different value', () {
+        // ARRANGE
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        var vector = Vector(0, 1);
+
+        int x = 0;
+        int y = 0;
+
+        var tile = Tile(2, x: x, y: y);
+        var blockingTile = Tile(4, x: 0, y: 3);
+
+        // Starting board
+        // |2|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |4|0|0|0|
+
+        // Ending board
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |2|0|0|0|
+        // |4|0|0|0|
+
+        // put the tiles in the board
+        board.tiles[x][y] = tile;
+        board.tiles[0][3] = blockingTile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, x);
+        expect(actual.y, 2);
+        expect(actual.merged, false);
+      });
+
+      test('should move to the right until blocked by another tile with a different value', () {
+        // ARRANGE
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        var vector = Vector(1, 0);
+
+        int x = 0;
+        int y = 0;
+
+        var tile = Tile(2, x: x, y: y);
+        var blockingTile = Tile(4, x: 3, y: 0);
+
+        // Starting board
+        // |2|0|0|4|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+
+        // Ending board
+        // |0|0|2|4|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+
+        // put the tiles in the board
+        board.tiles[x][y] = tile;
+        board.tiles[3][0] = blockingTile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, 2);
+        expect(actual.y, y);
+        expect(actual.merged, false);
+      });
+
+      test('should move to the left until blocked by another tile with a different value', () {
+        // ARRANGE
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        var vector = Vector(-1, 0);
+
+        int x = 3;
+        int y = 0;
+
+        var tile = Tile(2, x: x, y: y);
+        var blockingTile = Tile(4, x: 0, y: 0);
+
+        // Starting board
+        // |4|0|0|2|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+
+        // Ending board
+        // |4|2|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+
+        // put the tiles in the board
+        board.tiles[x][y] = tile;
+        board.tiles[0][0] = blockingTile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, 1);
+        expect(actual.y, y);
+        expect(actual.merged, false);
+      });
+    });
+    group('move with merge', () {
+      test('should move to the left and merge with the blocking tile', () {
+        // ARRANGE
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        var vector = Vector(-1, 0);
+
+        int x = 3;
+        int y = 0;
+
+        var tile = Tile(2, x: x, y: y);
+        var blockingTile = Tile(2, x: 0, y: 0);
+
+        // Starting board
+        // |2|0|0|2|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+
+        // Ending board
+        // |4|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+
+        // put the tiles in the board
+        board.tiles[x][y] = tile;
+        board.tiles[0][0] = blockingTile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, 0);
+        expect(actual.y, y);
+        expect(actual.merged, true);
+      });
+      test('should move to the right and merge with the blocking tile', () {
+        // ARRANGE
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        var vector = Vector(1, 0);
+
+        int x = 0;
+        int y = 0;
+
+        var tile = Tile(2, x: x, y: y);
+        var blockingTile = Tile(2, x: 3, y: 0);
+
+        // Starting board
+        // |2|0|0|2|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+
+        // Ending board
+        // |0|0|0|4|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+
+        // put the tiles in the board
+        board.tiles[x][y] = tile;
+        board.tiles[3][0] = blockingTile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, 3);
+        expect(actual.y, y);
+        expect(actual.merged, true);
+      });
+      test('should move down and merge with the blocking tile', () {
+        // ARRANGE
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        var vector = Vector(0, 1);
+
+        int x = 0;
+        int y = 0;
+
+        var tile = Tile(2, x: x, y: y);
+        var blockingTile = Tile(2, x: 0, y: 3);
+
+        // Starting board
+        // |2|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |2|0|0|0|
+
+        // Ending board
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |4|0|0|0|
+
+        // put the tiles in the board
+        board.tiles[x][y] = tile;
+        board.tiles[0][3] = blockingTile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, x);
+        expect(actual.y, 3);
+        expect(actual.merged, true);
+      });
+      test('should move up and merge with the blocking tile', () {
+        // ARRANGE
+        var tiles = List<List<Tile>>.generate(4, (y) => List(4));
+        var board = Board(tiles);
+
+        var vector = Vector(0, -1);
+
+        int x = 0;
+        int y = 3;
+
+        var tile = Tile(2, x: x, y: y);
+        var blockingTile = Tile(2, x: 0, y: 0);
+
+        // Starting board
+        // |2|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |2|0|0|0|
+
+        // Ending board
+        // |4|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+        // |0|0|0|0|
+
+        // put the tiles in the board
+        board.tiles[x][y] = tile;
+        board.tiles[0][0] = blockingTile;
+
+        // ACT
+        var actual = board.getTileDestination(tile, vector);
+        // ASSERT
+        expect(actual.x, x);
+        expect(actual.y, 0);
+        expect(actual.merged, true);
+      });
     });
   });
 }
