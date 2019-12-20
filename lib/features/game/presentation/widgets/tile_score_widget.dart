32b1b05a4e00c172b2edc4d317386b617a351f90
KiritchoukC <clement.kiritchouk@gmail.com>
2019-12-20 15:39:18 +0100

    extract tile score in widget

---
+++ lib/features/game/presentation/widgets/tile_score_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class TileScoreWidget extends StatelessWidget {
+  final int point;
+
+  const TileScoreWidget({Key key, this.point}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 5.0),
+      child: Text(
+        point.toString(),
+        semanticsLabel: 'A tile of value $point',
+        style: TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey.shade900,
+            shadows: [
+              Shadow(
+                blurRadius: 5.0,
+                color: Colors.grey.shade100.withOpacity(0.3),
+                offset: Offset(1.0, 1.0),
+              )
+            ]),
+      ),
+    );
+  }
+}
